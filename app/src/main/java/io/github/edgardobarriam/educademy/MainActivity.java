06bd217f389d7c736632245a32063ba39f69e3a4
Edgardo Barría Melián <edgardo.barriam@gmail.com>
2017-11-15 18:48:46 -0300

    Add Toolbar to MainActivity

--- app/src/main/java/io/github/edgardobarriam/educademy/MainActivity.java
+++ app/src/main/java/io/github/edgardobarriam/educademy/MainActivity.java
@@ -2,12 +2,19 @@ package io.github.edgardobarriam.educademy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivity();
+    }
+
+    private void initActivity() {
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
