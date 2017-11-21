4b1191d889cbfdbccc082984057a00ecd8fb54ce
Edgardo Barría Melián <edgardo.barriam@gmail.com>
2017-11-20 22:32:58 -0300

    Add splash screen, changed launcher icon

---
+++ app/src/main/java/io/github/edgardobarriam/educademy/SplashActivity.java
@@ -0,0 +1,21 @@
+package io.github.edgardobarriam.educademy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
